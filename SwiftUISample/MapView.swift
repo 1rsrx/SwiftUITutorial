32abeb70e435a165200193d909f4f0b2cf5bb136
HikaruKuroda <h1kqaa@gmail.com>
2021-06-14 09:55:19 +0900

    First

---
+++ SwiftUISample/MapView.swift
@@ -0,0 +1,29 @@
+//
+//  MapView.swift
+//  SwiftUISample
+//
+//  Created by HikaruKuroda on 2021/06/14.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let cordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: cordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
---
+++ SwiftUISample/SwiftUISampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUISampleApp.swift
+//  SwiftUISample
+//
+//  Created by HikaruKuroda on 2021/06/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+//            MapView()
+        }
+    }
+}
